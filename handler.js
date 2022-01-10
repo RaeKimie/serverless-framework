7ed0c8b4bcf1de0343f7eafe964dd577a642e1c8
RaeKimie <nr.kim91@gmail.com>
2022-01-10 13:11:10 +0000

    create serverless.yml with aws node template. added two functions to test

---
+++ handler.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  const passedParam = event.pathParameters.param;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: passedParam,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.cronTest = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'cronTest works!',
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
